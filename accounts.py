e5d3321849e73123af5d5f84c0f06ea482c2cb82
cthieman1 <cthieman@unomaha.edu>
2023-11-28 00:06:51 -0600

    added deposit function and further functionality

--- accounts.py
+++ accounts.py
@@ -10,13 +10,31 @@ class Accounts(QMainWindow, Ui_MainWindow):
         self.__account_balance = balance
 
         self.set_balance_button.clicked.connect(lambda : self.set_balance())
+        self.deposit_button.clicked.connect(self.deposit)
 
-        self.set_balance(0)
+
+        self.set_balance()
 
     def display_balance(self):
-        self.balance_label.setText(f'Balance: {self.__account_balance}')
+        self.balance_label.setText(f'Balance: {self.get_balance()}')
 
-    def set_balance(self, value=0):
+    def change_balance(self, value=0):
+        try:
+            value = float(value)
+        except ValueError:
+            value = 0
+
+        self.user_entry.clear()
+
+        if value > 0:
+            self.__account_balance = value
+        else:
+            self.__account_balance = 0
+
+        self.display_balance()
+
+
+    def set_balance(self):
         try:
             value = float(self.user_entry.text())
         except ValueError:
@@ -31,8 +49,20 @@ class Accounts(QMainWindow, Ui_MainWindow):
 
         self.display_balance()
 
+    def get_balance(self):
+        return self.__account_balance
 
 
+    def deposit(self):
+        try:
+            amount = float(self.user_entry.text())
+        except ValueError:
+            #TODO create prompts for blank or incorrect submissions
+            amount = 0
 
+        self.user_entry.clear()
+        if amount > 0:
+            self.change_balance(self.get_balance() + amount)
 
+        self.display_balance()
 
